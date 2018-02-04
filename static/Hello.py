82d633e80903d243185643f0754ce6e747278e0e
wxgaly <w962831870@gmail.com>
2018-02-05 00:16:23 +0800

    学习python --list、tuple、dict、set、for循环等

--- static/Hello.py
+++ static/Hello.py
@@ -5,10 +5,42 @@ a = ["a", 2.0]
 names.append(a)
 names.insert(1, "hah")
 
-print("before names: ", names)
+# print("before names: ", names)
 
-names.pop()
-print("after names: ", names)
+# names.pop()
+# print("after names: ", names)
 
 # tuple 有序元组（不可修改元素）
 classes = {"class one", "class two"}
+
+# s = input('birth: ')
+# birth = int(s)
+# if birth < 2000:
+#     print('00前')
+# else:
+#     print('00后')
+
+# for循环
+# for name in names:
+#     print(name)
+#
+# for cls in classes:
+#     print(cls)
+
+# sum = 0
+#
+# for i in range(101):
+#     sum += i
+# print(sum)
+
+# dictionary字典即java中的map
+# d = {'a': 1, "b": 2}
+# print(d["a"])
+#
+# d["a"] = 10
+#
+# print(d["a"])
+
+# set 不存在重复的元素
+s = set([1, 2, 2, 3, 3])
+print(s)
