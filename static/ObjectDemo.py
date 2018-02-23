c779ed411bd2edc2f46867c950a5de3cc30cd999
wxgaly <w962831870@gmail.com>
2018-02-24 00:33:46 +0800

    学习python --枚举类

--- static/ObjectDemo.py
+++ static/ObjectDemo.py
@@ -4,25 +4,50 @@
 
 __author__ = 'WXGALY'
 
+# class Student(object):
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def print_age(self):
+#         print('%s: %s' % (self.name, self.age))
+#
+#     def get_grade(self):
+#         if self.age >= 20:
+#             return 'A'
+#         elif self.age >= 40:
+#             return 'B'
+#         else:
+#             return 'C'
+#
+#
+# stu1 = Student("bb", 14)
+#
+# print(stu1.get_grade())
 
-class Student(object):
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# 枚举类型
 
-    def print_age(self):
-        print('%s: %s' % (self.name, self.age))
+from enum import Enum, unique
 
-    def get_grade(self):
-        if self.age >= 20:
-            return 'A'
-        elif self.age >= 40:
-            return 'B'
-        else:
-            return 'C'
+# Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+#
+# for name, member in Month.__members__.items():
+#     print(name, '=>', member, ',', member.value)
 
 
-stu1 = Student("bb", 14)
+@unique
+class Weekday(Enum):
+    Sun = 0  # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
 
-print(stu1.get_grade())
+
+day1 = Weekday.Sun
+
+print(day1)
