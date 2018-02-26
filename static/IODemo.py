e89681f53f63077ff34025c35da8a43bc404fe19
wxgaly <w962831870@gmail.com>
2018-02-26 21:48:54 +0800

    学习python --文件操作等

--- static/IODemo.py
+++ static/IODemo.py
@@ -13,10 +13,52 @@ __author__ = "WXGALY"
 #     #     print(line.strip())
 #     f.write('Hello, world!')
 
-from io import StringIO
 
-f = StringIO()
+# StringIO and ByteIO
+# from io import StringIO
+#
+# f = StringIO()
+#
+# f.write("hello")
+#
+# print(f.getvalue())
 
-f.write("hello")
+# from io import BytesIO
+#
+# f = BytesIO()
+#
+# f.write('中文'.encode("utf-8"))
+#
+# print(f.getvalue())
 
-print(f.getvalue())
+
+# 操作文件和目录
+
+import os
+
+# print(os.name)
+# print(os.uname())  # 注意uname()函数在Windows上不提供，也就是说，os模块的某些函数是跟操作系统相关的。
+
+# print(os.environ.get('PATH'))
+
+# print(os.path.abspath("."))
+
+# print(os.path.join(os.path.abspath("."), "test"))
+
+# path = os.path.join(os.path.abspath("."), "test")
+
+# os.mkdir(path)
+
+# os.rmdir(path)
+
+path = 'D:/Code/intellij_workspace/PythonDemo/static/'
+name = 'IODemo.py'
+
+# print(os.path.split(path + name))
+# print(os.path.splitext(path + name))
+
+# os.rename()
+# os.remove()
+
+# print([x for x in os.listdir('.') if os.path.isdir(x)])
+print([x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1] == '.py'])
