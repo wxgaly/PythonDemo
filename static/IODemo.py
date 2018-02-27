7e741d3a0dd1d5ffc825530c4fb8a8b41c817d7d
wangxuguang <nova_wangxuguang@126.com>
2018-02-27 17:01:01 +0800

    学习python --IO读写文件

--- static/IODemo.py
+++ static/IODemo.py
@@ -34,7 +34,7 @@ __author__ = "WXGALY"
 
 # 操作文件和目录
 
-import os
+# import os
 
 # print(os.name)
 # print(os.uname())  # 注意uname()函数在Windows上不提供，也就是说，os模块的某些函数是跟操作系统相关的。
@@ -51,8 +51,8 @@ import os
 
 # os.rmdir(path)
 
-path = 'D:/Code/intellij_workspace/PythonDemo/static/'
-name = 'IODemo.py'
+# path = 'D:/Code/intellij_workspace/PythonDemo/static/'
+# name = 'IODemo.py'
 
 # print(os.path.split(path + name))
 # print(os.path.splitext(path + name))
@@ -61,4 +61,54 @@ name = 'IODemo.py'
 # os.remove()
 
 # print([x for x in os.listdir('.') if os.path.isdir(x)])
-print([x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1] == '.py'])
+# print([x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1] == '.py'])
+
+
+# 序列化
+# import pickle
+#
+# path = "C:/Users/Administrator/Desktop/a.txt"
+
+# with open(path, 'wb') as f:
+#     d = dict(name='Bob', age=20, score=88)
+#
+#     pickle.dump(d, f)
+
+# with open(path, "rb") as f:
+#     d = pickle.load(f)
+#     print(d)
+
+
+# Json
+
+import json
+
+
+# d = dict(name='Bob', age=20, score=88)
+#
+# print(json.dumps(d))
+
+# json_str = '{"age": 20, "score": 88, "name": "Bob"}'
+#
+# print(json.loads(json_str))
+
+
+class Student(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+
+stu = Student('xiaoming', 18, 90)
+
+jsonStr = json.dumps(stu, default=lambda obj: obj.__dict__)
+
+
+def dict2student(d):
+    return Student(d['name'], d['age'], d['score'])
+
+
+stu = json.loads(jsonStr, object_hook=dict2student)
+
+print(stu.name)
