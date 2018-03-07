4a13bbf36b68e47c0db7f578e4954f1f98223ea6
wxgaly <w962831870@gmail.com>
2018-03-08 00:43:29 +0800

    学习python -- 异步IO，协程等

---
+++ static/asyncio/AsyncioDemo.py
@@ -0,0 +1,57 @@
+"""
+   date: 2018-03-07 22:52 
+"""
+
+__author__ = "WXGALY"
+
+# asyncio
+
+import asyncio
+
+
+# @asyncio.coroutine
+# def hello():
+#     print("Hello world!")
+#     # 异步调用asyncio.sleep(1):
+#     r = yield from asyncio.sleep(1)
+#     print("Hello again!")
+
+# 语法简洁，逻辑清晰
+async def hello():
+    print("Hello world!")
+    r = await asyncio.sleep(1)
+    print("Hello again!")
+
+
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+# 执行coroutine
+# loop.run_until_complete(hello())
+
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+
+loop.close()
+
+# 测试异步IO
+# @asyncio.coroutine
+# def wget(host):
+#     print('wget %s...' % host)
+#     connect = asyncio.open_connection(host, 80)
+#     reader, writer = yield from connect
+#     header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+#     writer.write(header.encode('utf-8'))
+#     yield from writer.drain()
+#     while True:
+#         line = yield from reader.readline()
+#         if line == b'\r\n':
+#             break
+#         print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+#     # Ignore the body, close the socket
+#     writer.close()
+#
+#
+# loop = asyncio.get_event_loop()
+# tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
