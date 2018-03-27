0e9a29c387f564b9eca11aadbc9a856c7f30aa94
wxgaly <w962831870@gmail.com>
2018-03-28 00:20:36 +0800

    学习python -- 编写web访问框架及配置文件

---
+++ static/webapp/conf/config_default.py
@@ -0,0 +1,18 @@
+"""
+   date: 2018-03-28 0:18 
+"""
+
+__author__ = "WXGALY"
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'wxg',
+        'password': '123456',
+        'database': 'wxg'
+    },
+    'session': {
+        'secret': 'WXG'
+    }
+}
---
+++ static/webapp/web/static/handlers.py
@@ -0,0 +1,7 @@
+"""
+   date: 2018-03-28 0:17 
+"""
+
+__author__ = "WXGALY"
+
+' url handlers'
